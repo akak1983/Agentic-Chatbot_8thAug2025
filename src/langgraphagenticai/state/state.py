47516d3ffb695364ca12bc3530187335673ec6c2
akak1983 <ashokkumar.kothandapani@gmail.com>
2025-08-08 23:32:34 -0400

    End to End Agentic app

---
+++ src/langgraphagenticai/state/state.py
@@ -0,0 +1,33 @@
+from typing_extensions import TypedDict,List
+from langgraph.graph.message import add_messages
+from typing import Annotated
+
+
+class State(TypedDict):
+    """
+    Represent the structure of the state used in graph
+    """
+    messages: Annotated[List,add_messages]
+
+    def basic_chatbot_build_graph(self):
+        """
+        Builds a basic chatbot graph using LangGraph.
+        This method initializes a chatbot node using the `BasicChatbotNode` class 
+        and integrates it into the graph. The chatbot node is set as both the 
+        entry and exit point of the graph.
+        """
+
+        self.basic_chatbot_node=BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_edge("chatbot",END)
+
+    def setup_graph(self, usecase: str):
+        """
+        Sets up the graph for the selected use case.
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_build_graph()
+
+        return self.graph_builder.compile()
